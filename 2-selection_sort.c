9a37d8c90b2df62e494214d1e842c944eb6c4566
root <root@DESKTOP-VS17LS8.localdomain>
2024-12-11 13:20:47 +0100

    Task Selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Selection Sort Algo
+ * @array: int array
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp, min_index;
+	long unsigned int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		temp = array[i];
+		array[i] = array[min_index];
+		array[min_index] = temp;
+	}
+}
