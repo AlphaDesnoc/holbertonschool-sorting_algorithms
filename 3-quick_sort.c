3580c1df44ec9364119b929d9c08ec5f1e125054
root <root@DESKTOP-VS17LS8.localdomain>
2024-12-12 21:44:46 +0100

    Task fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,15 +1,81 @@
 #include "sort.h"
 
 /**
- * quick_sort - Sorts an array of integers in ascending order using Quick sort.
- * @array: Pointer to the array to sort.
- * @size: Size of the array.
+ * swap -swaps two elements
+ * @a: first element
+ * @b: second element
  */
-void quick_sort(int *array, size_t size)
+void swap(int *a, int *b)
 {
-	if (array == NULL || size < 2)
-		return;
-
-	quick_sort_helper(array, 0, size - 1, size);
+int temp = *a;
+*a = *b;
+*b = temp;
 }
 
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * @array: array to be partitioned
+ * @low: first element of the array
+ * @high: last element of the array
+ * @size: size of array
+ * Return: index of pivot
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+int pivot, i, j;
+
+pivot = array[high];
+i = low - 1;
+
+for (j = low; j < high; j++)
+{
+if (array[j] <= pivot)
+{
+i++;
+if (i != j)
+{
+swap(&array[i], &array[j]);
+print_array(array, size);
+}
+}
+}
+if (array[i + 1] != array[high])
+{
+swap(&array[i + 1], &array[high]);
+print_array(array, size);
+}
+return (i + 1);
+}
+
+/**
+ * quick_sort_helper- sorts array using quick sort algorithm
+ * @array: array to be sorted
+ * @low: starting index of the array
+ * @high: ending index of the array
+ * @size: size of array
+ */
+static void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+int pivot;
+
+if (low < high)
+{
+pivot = lomuto_partition(array, low, high, size);
+quick_sort_helper(array, low, pivot - 1, size);
+quick_sort_helper(array, pivot + 1, high, size);
+}
+}
+
+/**
+ * quick_sort - sotrs array using quick sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+
+void quick_sort(int *array, size_t size)
+{
+if (array == NULL || size < 2)
+return;
+
+quick_sort_helper(array, 0, (int)size - 1, size);
+}
